541784047eb54fac8baa9ab7fb914a72c08a7272
Kuba <jakub.grzeda@protonmail.com>
2025-06-29 13:21:22 +0200

    DatePicker implemented

--- app/src/main/java/com/kuba/calendarium/ui/screens/CalendarScreen.kt
+++ app/src/main/java/com/kuba/calendarium/ui/screens/CalendarScreen.kt
@@ -1,15 +1,53 @@
 package com.kuba.calendarium.ui.screens
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import java.util.Date
 
 @Composable
 fun CalendarScreen(viewModel: CalendarViewModel) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Text(viewModel.uiState.collectAsState().value.text)
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Calendar(
+            date = viewModel.uiState.collectAsState().value.selectedDate,
+            onDateSelected = { viewModel.onEvent(CalendarViewModel.UIEvent.DateSelected(it)) }
+        )
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun Calendar(
+    date: Date,
+    onDateSelected: (Date) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val datePickerState = rememberDatePickerState(
+        initialSelectedDateMillis = date.time
+    )
+
+    LaunchedEffect(datePickerState.selectedDateMillis) {
+        snapshotFlow { datePickerState.selectedDateMillis }
+            .filterNotNull()
+            .distinctUntilChanged()
+            .map { Date(it) }
+            .collect(onDateSelected)
+    }
+
+    DatePicker(
+        state = datePickerState,
+        modifier = modifier,
+        title = { }
+    )
 }
--- app/src/main/java/com/kuba/calendarium/ui/screens/CalendarViewModel.kt
+++ app/src/main/java/com/kuba/calendarium/ui/screens/CalendarViewModel.kt
@@ -4,15 +4,28 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import java.util.Date
 import javax.inject.Inject
 
 @HiltViewModel
 class CalendarViewModel @Inject constructor() : ViewModel() {
-    private val _uiState = MutableStateFlow(UiState())
+    private val _uiState = MutableStateFlow(UIState())
 
     val uiState = _uiState.asStateFlow()
 
-    data class UiState(
-        val text: String = "ViewModel initialized"
+    fun onEvent(event: UIEvent) {
+        when (event) {
+            is UIEvent.DateSelected -> {
+                _uiState.value = _uiState.value.copy(selectedDate = event.date)
+            }
+        }
+    }
+
+    data class UIState(
+        val selectedDate: Date = Date()
     )
+
+    sealed class UIEvent {
+        data class DateSelected(val date: Date) : UIEvent()
+    }
 }
