d554c91683de2c18bc231dc12f293e640169c720
Kuba <jakub.grzeda@protonmail.com>
2025-06-30 21:10:45 +0200

    Instrumental test checking if Event.Done adds item to database

--- app/src/androidTest/java/com/kuba/calendarium/ui/screens/addEvent/AddEventViewModelTest.kt
+++ app/src/androidTest/java/com/kuba/calendarium/ui/screens/addEvent/AddEventViewModelTest.kt
@@ -2,10 +2,13 @@ package com.kuba.calendarium.ui.screens.addEvent
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.test.core.app.ActivityScenario
+import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
 import com.kuba.calendarium.DummyHiltActivity
+import com.kuba.calendarium.util.resetToMidnight
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -15,6 +18,9 @@ import org.junit.Test
 class AddEventViewModelTest {
     @get:Rule
     val hiltRule = HiltAndroidRule(this)
+
+    private lateinit var viewModel: AddEventViewModel
+
 
     @Before
     fun setUp() {
@@ -28,6 +34,39 @@ class AddEventViewModelTest {
                 val viewModel = ViewModelProvider(it)[AddEventViewModel::class.java]
                 assertThat(viewModel).isNotNull()
                 assertThat(viewModel.eventsRepository).isNotNull()
+            }
+        }
+    }
+
+    @Test
+    fun testDoneEventInsertingDataToDatabase() {
+        ActivityScenario.launch(DummyHiltActivity::class.java).use { scenario ->
+            scenario.onActivity {
+                val viewModel = ViewModelProvider(it)[AddEventViewModel::class.java]
+                val date = System.currentTimeMillis()
+
+                runTest {
+                    viewModel.onEvent(AddEventViewModel.UIEvent.TitleChanged("Test Title"))
+                    viewModel.onEvent(AddEventViewModel.UIEvent.DescriptionChanged("Test Description"))
+                    viewModel.onEvent(AddEventViewModel.UIEvent.DateSelected(date))
+
+                    viewModel.onEvent(AddEventViewModel.UIEvent.DoneClicked)
+
+                    viewModel.eventsRepository.getEventsForDate(date.resetToMidnight()).test {
+                        val events = awaitItem()
+                        assertThat(events).hasSize(1)
+
+                        val event1 = events.firstOrNull()
+
+                        assertThat(event1).isNotNull()
+                        assertThat(event1?.title).isEqualTo("Test Title")
+                        assertThat(event1?.description).isEqualTo("Test Description")
+                        assertThat(event1?.date).isEqualTo(date.resetToMidnight())
+
+                        expectNoEvents()
+                        cancelAndConsumeRemainingEvents()
+                    }
+                }
             }
         }
     }
