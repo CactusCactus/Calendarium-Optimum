0c6c38d3b0c8ee3987ac50ea47ecfe40175e721f
Kuba <jakub.grzeda@protonmail.com>
2025-07-05 07:43:20 +0200

    Setting option for showing delete dialog

--- app/src/main/java/com/kuba/calendarium/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/kuba/calendarium/ui/screens/settings/SettingsScreen.kt
@@ -1,14 +1,24 @@
 package com.kuba.calendarium.ui.screens.settings
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.kuba.calendarium.R
+import com.kuba.calendarium.ui.common.StandardSpacer
+import com.kuba.calendarium.ui.common.standardPadding
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -16,6 +26,28 @@ fun SettingsScreen(viewModel: SettingsViewModel) {
     Scaffold(topBar = {
         TopAppBar(title = { Text("Settings", style = MaterialTheme.typography.displayMedium) })
     }) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) { }
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(standardPadding)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    stringResource(R.string.settings_show_delete_dialog_text),
+                    style = MaterialTheme.typography.titleMedium
+                )
+
+                StandardSpacer()
+
+                Switch(
+                    checked = viewModel.uiState.collectAsState().value.showDeleteDialog,
+                    onCheckedChange = { viewModel.onEvent(UIEvent.DeleteDialogChanged(it)) }
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/kuba/calendarium/ui/screens/settings/SettingsViewModel.kt
+++ app/src/main/java/com/kuba/calendarium/ui/screens/settings/SettingsViewModel.kt
@@ -1,11 +1,43 @@
 package com.kuba.calendarium.ui.screens.settings
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kuba.calendarium.data.dataStore.UserPreferencesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingsViewModel @Inject constructor(userPreferencesRepository: UserPreferencesRepository) :
-    ViewModel() {
+class SettingsViewModel @Inject constructor(
+    private val userPreferencesRepository: UserPreferencesRepository
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(UIState())
+
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            userPreferencesRepository.getShowDialogDeletePreference().collect { show ->
+                _uiState.update { _uiState.value.copy(showDeleteDialog = show) }
+            }
+        }
+    }
+
+    fun onEvent(event: UIEvent) = when (event) {
+        is UIEvent.DeleteDialogChanged -> viewModelScope.launch {
+            userPreferencesRepository.setShowDialogDeletePreference(event.show)
+            _uiState.update { _uiState.value.copy(showDeleteDialog = event.show) }
+        }
+    }
+}
+
+data class UIState(
+    val showDeleteDialog: Boolean = false
+)
+
+sealed class UIEvent {
+    data class DeleteDialogChanged(val show: Boolean) : UIEvent()
 }
