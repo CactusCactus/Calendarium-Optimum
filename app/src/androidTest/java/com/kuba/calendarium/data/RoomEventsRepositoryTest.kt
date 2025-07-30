bf54a69df43be0eb108b61aff00ef29b3a50b672
Kuba <jakub.grzeda@protonmail.com>
2025-07-30 10:24:48 +0200

    Repetition db test

--- app/src/androidTest/java/com/kuba/calendarium/data/RoomEventsRepositoryTest.kt
+++ app/src/androidTest/java/com/kuba/calendarium/data/RoomEventsRepositoryTest.kt
@@ -9,6 +9,7 @@ import com.google.common.truth.Truth.assertThat
 import com.kuba.calendarium.data.dao.EventDao
 import com.kuba.calendarium.data.model.Event
 import com.kuba.calendarium.data.model.Task
+import com.kuba.calendarium.data.model.internal.Repetition
 import com.kuba.calendarium.data.repo.EventsRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -419,6 +420,29 @@ class RoomEventsRepositoryTest {
             emittedEvent?.tasks?.forEach {
                 assertThat(it.done).isTrue()
             }
+
+            cancelAndConsumeRemainingEvents()
+        }
+    }
+
+    @Test
+    fun insertARepeatingEventCheckForItInTheFuture() = runTest {
+        val eventId = "insertARepeatingEventCheckForItInTheFuture".hashCode().toLong()
+        val date = LocalDate.now()
+        val dateInAWeek = date.plusDays(7)
+
+        val event = Event(
+            id = eventId,
+            title = "Test Event",
+            description = "This is a test event",
+            date = date,
+            repetition = Repetition.WEEKLY
+        )
+
+        repository.insertEvent(event)
+        repository.getEventTasksListForDate(dateInAWeek).test {
+            val emittedEvents = awaitItem()
+            assertThat(emittedEvents.map { it.event }).containsExactly(event)
 
             cancelAndConsumeRemainingEvents()
         }
